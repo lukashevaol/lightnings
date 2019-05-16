6f10405aea0b7fbeff5054027e59f79c90441856
lukashevaol <lukashevaol@gmail.com>
2019-05-17 02:22:17 +0300

    removed drafts

--- models.py
+++ models.py
@@ -1,25 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date, DateTime
-from sqlalchemy.dialects.postgresql import MONEY
+from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
 import pytz
 
 Base = declarative_base()
-
-
-class Book(Base):
-    __tablename__ = 'books'
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    author = Column(String)
-    pages = Column(Integer)
-    published = Column(Date)
-    description = Column(String)
-    price = Column(MONEY)
-
-    def __repr__(self):
-        return "<Book(title='{}', author='{}', pages={}, published={})>" \
-            .format(self.title, self.author, self.pages, self.published)
 
 
 class Coordinate(Base):
